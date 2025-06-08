a21dfff186b5257ce26ad54a7c5882bb3d20bc1c
Vishesh Thakral <thakralvishesh@gmail.com>
2025-06-08 22:56:19 +0530

    Added alias for @

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@gsap/react', 'gsap']
-  },
-  build: {
-    commonjsOptions: {
-      include: [/@gsap\/react/, /node_modules/]
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     }
   }
 })
