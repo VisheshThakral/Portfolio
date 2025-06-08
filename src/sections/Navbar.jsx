6d946493bd0e1a09449dd342545434a6c2dffba7
Vishesh Thakral <thakralvishesh@gmail.com>
2025-06-08 23:29:19 +0530

    Added a menu icon for responsiveness

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { navLinks } from '../constants/index.js';
 
 const NavItems = () => (
@@ -13,6 +14,10 @@ const NavItems = () => (
 );
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
       <div className="max-w-7xl mx-auto">
@@ -21,15 +26,22 @@ const Navbar = () => {
             Vishesh Thakral
           </a>
 
+          <button
+            onClick={toggleMenu}
+            className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
+            aria-label="Toggle menu">
+            <img src={isOpen ? 'assets/close.svg' : 'assets/menu.svg'} alt="toggle" className="w-6 h-6" />
+          </button>
+
           <nav className="sm:flex hidden">
             <NavItems />
           </nav>
         </div>
       </div>
 
-      <div className={`nav-sidebar max-h-screen`}>
+      <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
         <nav className="p-5">
-          <NavItems />
+          <NavItems onClick={closeMenu} />
         </nav>
       </div>
 
